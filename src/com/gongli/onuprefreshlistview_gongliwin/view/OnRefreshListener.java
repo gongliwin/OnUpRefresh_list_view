d08317ea2e4d61e299e3d5888c4183073a68eb9c
gongliwin <1205296390@qq.com>
2014-11-03 12:52:27 +0800

    init

---
+++ src/com/gongli/onuprefreshlistview_gongliwin/view/OnRefreshListener.java
@@ -0,0 +1,14 @@
+package com.gongli.onuprefreshlistview_gongliwin.view;
+
+public interface OnRefreshListener {
+
+	/**
+	 * 当是下拉刷新时, 回调此方法, 进行刷新数据的操作
+	 */
+	public void OnPullDownRefresh();
+
+	/**
+	 * 当前是加载更多的操作.
+	 */
+	public void onLoadingMore();
+}
